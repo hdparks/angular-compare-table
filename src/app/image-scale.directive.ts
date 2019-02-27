adffa50fecc4b07e1b2508f53508ddfe52ee424d
Hayden Parkinson <haydendparkinson@gmail.com>
2019-02-27 13:17:36 -0700

    tall-wide rescaling done

--- src/app/image-scale.directive.ts
+++ src/app/image-scale.directive.ts
@@ -1,27 +1,18 @@
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appImageScale]'
 })
 export class ImageScaleDirective {
 
-    e:ElementRef;
-    @HostBinding('style.width') width:string;
-    @HostBinding('style.height') height:string;
-
-    constructor(e:ElementRef, ){
-        this.e = e;
+    constructor(private renderer:Renderer2){
     }
 
-    @HostListener('load',['$event'])
-    onLoad(event:Event){
-        console.log(event)
-        if(this.width < this.height){
-            this.e.nativeElement.classList.add("tall");
-        } else {
-            this.e.nativeElement.classList.add("wide");
-        }
-
+    @HostListener('load',['$event.target'])
+    onLoad(el:ElementRef){
+        console.log(el.offsetHeight, el.offsetWidth)
+        let klass = el.offsetHeight > el.offsetWidth ? "tall" : "wide"
+        this.renderer.addClass(el, klass)
     }
 
 }
