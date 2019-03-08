1616fe2b2cec430ea550019ae6697d40037e6d5b
hdparks <haydendparkinson@gmail.com>
2019-03-08 13:38:35 -0700

    Production ready

--- src/app/view-data-modal/view-data-modal.component.ts
+++ src/app/view-data-modal/view-data-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { ReadJsonService } from '../read-json.service';
+import { DEVICES } from '../device-data'
 
 @Component({
   selector: 'app-view-data-modal',
@@ -10,22 +11,26 @@ import { ReadJsonService } from '../read-json.service';
 })
 export class ViewDataModalComponent implements OnInit {
 
+    public devices = DEVICES;
     public propertiesList: Observable<Object[]>;
 
     constructor(@Inject(MAT_DIALOG_DATA) public viewData: any, private readJsonService:ReadJsonService) {
         this.propertiesList = new Observable((observer)=>{
-            //  parses the view-specific data from the JSON file,
-            //  relays that out as an observable
-            this.readJsonService.getJSON().subscribe((jsonData)=>{
-                console.log("Got data!",jsonData, this.viewData.viewName)
-                observer.next(jsonData[this.viewData.viewName]);
-            });
+            //
             observer.next([{name:"Loading..."}])
+
+
+            //  relays the view-specific data from the JSON file
+            this.readJsonService.getJSON().subscribe((jsonData)=>{
+                console.log( "Got data!", jsonData, this.viewData.viewName )
+                observer.next( jsonData[ this.viewData.viewName ] );
+            });
         });
     }
 
 
     ngOnInit() {
+        console.log("List of devices:", DEVICES);
     }
 
 }
